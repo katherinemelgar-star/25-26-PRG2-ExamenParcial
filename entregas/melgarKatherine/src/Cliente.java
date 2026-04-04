ba02fef8d0e012ec1ceaa85a834dd50c250a90b8
katherinemelgar-star <katherine.melgar@alumnos.uneatlantico.es>
2026-04-04 14:11:06 +0000

    feat: agregar clases Banco, Cliente y Cuenta

---
+++ entregas/melgarKatherine/src/Cliente.java
@@ -0,0 +1,12 @@
+class Cliente{
+    private Banco banco 
+    private String nombre
+    private String dni
+    private String direccion
+
+    public Cliente(){}
+    public void actualizarNombre(){}
+    public void mostrar(){}
+    public void actualizarDni(){}
+    public void actualizarDireccion(){}
+}
---
+++ entregas/melgarKatherine/src/Cuenta.java
@@ -0,0 +1,11 @@
+class Cuenta{
+    private String numeroIBAN
+    private double saldo
+    private String tipoCuenta
+    public Cuenta(){}
+
+    public void movimientos(double cantidad, String fecha){}
+    public void ingresos(double cantidad, String fecha){}
+    public void retiradas(double cantidad, String fecha){}
+    public void transferencias(double cantidad, String fecha){}
+}
